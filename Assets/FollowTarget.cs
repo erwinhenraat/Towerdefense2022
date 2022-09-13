5df26366b54e480e59f8bf8e3c47b26690f03915
Erwin Henraat <e.henraat@ma-web.nl>
2022-09-13 18:35:48 +0200

    create script for turret, game objects for tower and enemy, fixed vscode dotnet extension

---
+++ Assets/FollowTarget.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowTarget : MonoBehaviour
+{
+    private Transform target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //List enemies
+        //target = GameObject.Find("enemy");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
